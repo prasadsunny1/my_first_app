fe4b2a91797c2fad428ab7ce0344d2b9b96016fa
sanni prasad <sanni@auberginesolutions.com>
2021-01-02 12:26:40 +0530

    Added futurebuilder to handle get movies
    show loading indicator while data is being fetched

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/movie_service.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home page'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Index' + " = " + index.toString()),
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Home page'),
+        ),
+        body: FutureBuilder<Map<dynamic, dynamic>>(
+          future: getMovies(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data.toString());
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        )
+
+        // ListView.builder(
+        //   itemBuilder: (context, index) {
+        //     return ListTile(
+        //       title: Text('Index' + " = " + index.toString()),
+        //     );
+        //   },
+        // ),
+        );
   }
 }
